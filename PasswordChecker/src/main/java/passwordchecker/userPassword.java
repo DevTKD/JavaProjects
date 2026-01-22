4f512a565239c84d54972a2b8ea26b657d439cab
Tanya Davis <tdavis_la@outlook.com>
2026-01-21 21:08:45 -0500

    Added logic for checking the password strength

--- PasswordChecker/src/main/java/passwordchecker/userPassword.java
+++ PasswordChecker/src/main/java/passwordchecker/userPassword.java
@@ -2,7 +2,6 @@ package passwordchecker;
 
 import java.util.Scanner;
 
-
 public class userPassword {
     private Scanner scanner = new Scanner(System.in);
 
@@ -17,11 +16,33 @@ public class userPassword {
             STRONG
         }
 
-        //User to input password
+        // User to input password
         public String getPassword() {
             System.out.print("Enter your password: ");
             return scanner.nextLine();
         }
 
+        // Check if password is strong, medium, or weak
+        public static PasswordStrength checkPassword(String password) {
+            boolean longEnough = password.length() >= 10;
+            boolean hasUppercase = !password.equals(password.toLowerCase());
+            boolean hasLowercase = !password.equals(password.toUpperCase());
+            boolean hasNumber = password.matches(".*\\d.*");
+            boolean hasSpecial = !password.matches("[a-zA-Z0-9]*");
+
+            if (longEnough && hasUppercase && hasLowercase && hasNumber && hasSpecial) {
+                return PasswordStrength.STRONG;
+            }
+
+            if (longEnough && hasUppercase && hasLowercase && hasNumber) {
+                return PasswordStrength.MEDIUM;
+            }
+
+            if (longEnough && hasUppercase && hasLowercase) {
+                return PasswordStrength.WEAK;
+            }
+
+            return PasswordStrength.WEAK; // Default return if password doesn't meet minimum requirements
+        }
     }
 }
